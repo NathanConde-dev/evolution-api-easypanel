ae494c8f82840ae93fc4e2c18482ad6d160cf816
Nathan Conde <82476114+NathanConde-dev@users.noreply.github.com>
2025-01-03 22:24:52 -0300

    Update Repositorio

--- src/installEvolutionAPI.js
+++ src/installEvolutionAPI.js
@@ -4,19 +4,19 @@ function installEvolutionAPI() {
   return new Promise((resolve, reject) => {
     console.log("Instalando Evolution API...");
 
-    // Comando para clonar e configurar Evolution API
-    exec(
-      "git clone https://github.com/atendai/evolution-api.git && cd evolution-api && npm install && npm run build",
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Erro ao instalar Evolution API: ${stderr}`);
-          reject(error);
-          return;
-        }
-        console.log("Evolution API instalada com sucesso.");
-        resolve(stdout);
+    // Comando para clonar o seu repositório
+    const repoUrl = "https://github.com/NathanConde-dev/evolution-api-conde.git";
+    const command = `git clone ${repoUrl} && cd evolution-api-conde && npm install && npm run build`;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Erro ao instalar Evolution API: ${stderr}`);
+        reject(error);
+        return;
       }
-    );
+      console.log("Evolution API instalada com sucesso.");
+      resolve(stdout);
+    });
   });
 }
 
